131fba6531da70c289e858a875b9b94d31c481a9
Ahmed Abdelbaset <A7med3bdulbaset@gmail.com>
2024-01-29 12:05:27 +0200

    .next

---
+++ apps/web/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"vrvyjMbhYcajRdgDNzkHfRveFGYzZYf1QsoFXBeMEKo=\"}"
---
+++ apps/web/.next/static/chunks/app/layout-d8b35506a3e18142.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{50:function(){}},function(n){n.O(0,[577,726,744],function(){return n(n.s=50)}),_N_E=n.O()}]);
